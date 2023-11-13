f27d2696c5335aba722634c51c3adcc0d108f6e2
SiddTripathi <siddhart.tripathi@gmail.com>
2023-11-13 18:04:17 +1300

    Day 1 - class,methods, inheritance

---
+++ Udemy_Python/Python_basics/Object_oriented/class_and_static_methd.py
@@ -0,0 +1,56 @@
+#instance,class and static method
+
+class ClassTest:
+    def instance_mthd(self):
+        print(f"Called instance method of {self}")
+    
+
+    @classmethod
+    def class_mthd(cls):
+        print(f"Called class method of {cls}")
+    
+    @staticmethod
+    def static_mthd():
+        print("Called Static method")
+
+
+test = ClassTest()
+test.instance_mthd()
+test.static_mthd()
+ClassTest.class_mthd()              #For Instance method, need to create object. But for class and static method, no need to create object. Though, Object can also access these methods 
+ClassTest.static_mthd()
+ClassTest.instance_mthd(test)
+
+
+#Example class method
+#Now in this book class, one can pass any book type(comic, action etc.) unless you make it clear somehow that book type can be hard cover or paper back nothing else. For this use class method. See how
+
+class Book:
+    BOOK_TYPE = ("hardcover","paperback")
+    def __init__(self,name,book_type,weight):
+        self.name =name
+        self.book_type=book_type
+        self.weight = weight
+    def __str__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}"
+    @classmethod
+    def hardcover(cls,name,page_weight):
+        return cls(name, cls.BOOK_TYPE[0],page_weight+100)
+    @classmethod
+    def paperback(cls,name,page_weight):
+        return cls(name, cls.BOOK_TYPE[1],page_weight)
+    
+    @staticmethod
+    def testing(book):
+        return f" static method prints {book.name}"
+    
+
+book =  Book("Harry Potter","hardcover",200)
+
+print(Book.testing(book))
+
+
+print(book.name)
+
+
+
